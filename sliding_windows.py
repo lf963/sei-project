b579f45dce8fa490cf3577b82d0eba8f8cc88fb8
sasuke11291 <41359956+sasuke11291@users.noreply.github.com>
2018-07-19 16:13:58 +0800

    Update sliding_windows.py

--- sliding_windows.py
+++ sliding_windows.py
@@ -16,14 +16,13 @@ import re
 
 def load_data():
     wiki = []
-    with open('/Users/garylai/Desktop/wiki2.csv', newline='') as csvfile:
+    with open('/Users/garylai/Desktop/wiki2.csv', newline = '') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        next(reader, None)
         for row in reader:
             print(row)
             wiki.append(row)
-    return wiki
-        
+    wiki_dict = dict((wi[0], wi[1:]) for wi in wiki)
+    return wiki_dict
         
 def input_word():
     print('請輸入欲搜尋的字詞')
@@ -132,31 +131,31 @@ def processing(length, is_doc, in_list, out_list):
         print(run)
         slide_in_list = []
         slide_out_list = []
-        
+        doc = str(doc)
         for check in in_list:
             if type(check) == str:
-                slide_in_list.append([m.start() for m in re.finditer(check,wiki[doc][3])])
+                slide_in_list.append([m.start() for m in re.finditer(check,wiki[doc][2])])
             else:
-                f_word = [m.start() for m in re.finditer(check[0],wiki[doc][3])]
-                s_word = [m.start() for m in re.finditer(check[1],wiki[doc][3])]
+                f_word = [m.start() for m in re.finditer(check[0],wiki[doc][2])]
+                s_word = [m.start() for m in re.finditer(check[1],wiki[doc][2])]
                 c_word = []
                 
                 for i in f_word:
                     for j in s_word:
-                        if abs(i-j) < check[2]:
+                        if abs(i-j) <= len(check[0])+check[2]:
                             c_word.append(i)
                 slide_in_list.append(c_word)
                 
         for check in out_list:
             if type(check) == str:
-                slide_out_list.append([m.start() for m in re.finditer(check,wiki[doc][3])])
+                slide_out_list.append([m.start() for m in re.finditer(check,wiki[doc][2])])
             else:
-                f_word = [m.start() for m in re.finditer(check[0],wiki[doc][3])]
-                s_word = [m.start() for m in re.finditer(check[1],wiki[doc][3])]
+                f_word = [m.start() for m in re.finditer(check[0],wiki[doc][2])]
+                s_word = [m.start() for m in re.finditer(check[1],wiki[doc][2])]
                 c_word = []
                 for i in f_word:
                     for j in s_word:
-                        if abs(i-j) < check[2]:
+                        if abs(i-j) <= len(check[0])+check[2]:
                             c_word.append(i)
                 slide_out_list.append(c_word)
         
@@ -202,18 +201,109 @@ def processing(length, is_doc, in_list, out_list):
                     get_docs.append(doc)
                     break
         run+=1
-    return get_docs        
+    return get_docs 
+
+
+'''----'''
+def processing(length, is_doc, in_list, out_list):
+    get_docs = []
+    run = 0
+    for doc in is_doc:
+        print(run)
+        slide_in_list = []
+        slide_out_list = []
+        
+        for check in in_list:
+            if type(check) == str:
+                slide_in_list.append([m.start() for m in re.finditer(check,wiki['690'][2])])
+            else:
+                f_word = [m.start() for m in re.finditer(check[0],wiki['690'][2])]
+                s_word = [m.start() for m in re.finditer(check[1],wiki['690'][2])]
+                c_word = []
+                
+                for i in f_word:
+                    for j in s_word:
+                        if abs(i-j) <= len(check[0])+check[2]:
+                            c_word.append(i)
+                slide_in_list.append(c_word)
+                
+        for check in out_list:
+            if type(check) == str:
+                slide_out_list.append([m.start() for m in re.finditer(check,wiki['690'][2])])
+            else:
+                f_word = [m.start() for m in re.finditer(check[0],wiki['690'][2])]
+                s_word = [m.start() for m in re.finditer(check[1],wiki['690'][2])]
+                c_word = []
+                for i in f_word:
+                    for j in s_word:
+                        if abs(i-j) <= len(check[0])+check[2]:
+                            c_word.append(i)
+                slide_out_list.append(c_word)
+        
+        if len([x for x in slide_out_list if x != []]) > 0:
+            end = max([max([sublist[-1] for sublist in slide_in_list]),max([sublist[-1] for sublist in slide_out_list if sublist != []])])
+            start = min([min([sublist[0] for sublist in slide_in_list]),min([sublist[0] for sublist in slide_out_list if sublist != []])])
+        else:
+            end = max([sublist[-1] for sublist in slide_in_list])
+            start = min([sublist[0] for sublist in slide_in_list])
+        
+        if (end-start <length) & (len([x for x in slide_out_list if x != []]) > 0):
+            pass
+        elif (end-start <length) & (len([x for x in slide_out_list if x != []]) == 0):
+            get_docs.append(doc)
+        else:
+            slide_doc = np.zeros((len(in_list)+len(out_list),end-start+1), dtype = bool)
+            
+            iter = 0
+            for sublist in slide_in_list:
+                if len(sublist) != 0:                    
+                    slide_doc[iter,np.asarray(sublist)-start] = True
+                else:
+                    pass
+                iter += 1  
+            for sublist in slide_out_list:
+                if len(sublist) != 0:                    
+                    slide_doc[iter,np.asarray(sublist)-start] = True
+                else:
+                    pass
+                iter += 1
+            
+            k = len(in_list)
+            l = len(out_list)
+            m = len(slide_doc[0])-length+1
+            
+            slide_doc_in = np.max(sliding_window(slide_doc[:len(in_list),:], size = length, stepsize=1),axis=2).reshape(k,m)
+            slide_doc_out = np.min(np.invert(sliding_window(slide_doc[len(in_list):,:], size = length, stepsize=1)),axis=2).reshape(l,m)
+            slide_doc_all = np.concatenate((slide_doc_in, slide_doc_out), axis=0)
+            
+            entre = np.where(slide_doc_all.sum(axis=1) == min(slide_doc_all.sum(axis=1)))[0][0]
+            for i in np.where(slide_doc_all[entre,:] == True)[0]:
+                if not(False in slide_doc_all[:,i]):
+                    get_docs.append(doc)
+                    break
+        run+=1
+    return get_docs           
 
 
 def get_doc(is_doc):
     title = []
     for i in is_doc:
         title.append(wiki[i][2])
-    return title, len(title)
-     
+    return title, is_doc
+
+
+def main():
+    t0 = time.time()
+    search_answer = []
+    for searching in input_processing:
+        length = searching[0]
+        is_doc = searching[1]
+        in_list = searching[2]
+        out_list = searching[3]
+        title, doc_index = get_doc(processing(length, is_doc, in_list, out_list))
+        search_answer.append(doc_index)
+    print(time.time()-t0)
+    
 
 if __name__ == '__main__':
-
-    t0 = time.time()
-    title, num = get_doc(processing(length,is_doc,in_list, out_list))
-    print(time.time()-t0)
+    main()
