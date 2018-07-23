516e1ae4d1083b5955ff52429cc1f166072a2c9e
lf963 <32009158+lf963@users.noreply.github.com>
2018-07-23 10:07:25 +0800

    SELECT from database batch by batch

--- and_or_output.py
+++ and_or_output.py
@@ -2,22 +2,32 @@ import sqlite3
 
 
 def print_result(search_result):
-    count = 0
+    result = []
+
+    # Because sqlite3 can accept only 1000 parameters, we fetch our result batch by batch
+    # If batch_size is 900 and we have 4123 items, we have to run (4123 / 900) + 1 times
+    batch_size = 900
+    batch_num = int(len(search_result) / batch_size) + 1
+
     if len(search_result) != 0:
         conn = sqlite3.connect('News.sqlite')
-        cur = conn.cursor()
-        sql = "SELECT id, url, title FROM source WHERE "
-        for index in search_result:
-            if search_result[-1] == index:
-                sql += "id = " + str(index)
-            else:
-                sql += "id = " + str(index) + " OR "
-        cur.execute(sql)
-        data = cur.fetchall()
-        for row in data:
-            count += 1
-            print(row[0], row[1], row[2])
-    print("Found %d results" %count)
+        for i in range(0, batch_num):
+            cur = conn.cursor()
+            sql = "SELECT id, url, title FROM source WHERE "
+            for j in range(i * batch_size, i*batch_size + batch_size):
+                if j >= len(search_result):
+                    break
+                if j == i * batch_size + batch_size - 1 or j == len(search_result) - 1:
+                    sql += "id = " + str(search_result[j])
+                else:
+                    sql += "id = " + str(search_result[j]) + " OR "
+            cur.execute(sql)
+            data = cur.fetchall()
+            for row in data:
+                result.append([row[0], row[1], row[2]])
+    for x in range(0, len(result)):
+        print(result[x])
+    print("Found %d results" % len(search_result))
 
 
 def and_operation(list1, list2):
