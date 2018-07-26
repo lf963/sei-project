ad7d25e1f45c98ea383e2c1df377321ef6050cb7
lf963 <32009158+lf963@users.noreply.github.com>
2018-07-26 15:09:13 +0800

    Add legend to graph

--- and_or_output.py
+++ and_or_output.py
@@ -49,11 +49,11 @@ def print_result(search_result):
     batch_num = int(len(search_result) / batch_size) + 1
 
     if len(search_result) != 0:
-        conn = sqlite3.connect(os.path.join(os.getcwd(),  'News.sqlite'))
+        conn = sqlite3.connect(os.path.join(os.getcwd(), 'News.sqlite'))
         for i in range(0, batch_num):
             cur = conn.cursor()
             sql = "SELECT id, url, title FROM wiki WHERE "
-            for j in range(i * batch_size, i*batch_size + batch_size):
+            for j in range(i * batch_size, i * batch_size + batch_size):
                 if j >= len(search_result):
                     break
                 if j == i * batch_size + batch_size - 1 or j == len(search_result) - 1:
@@ -96,11 +96,12 @@ def and_or(article_id_list, operator_list):
     node_index = -1
     node_prefix = 'last'
 
-    with dot.subgraph() as s:
+    with dot.subgraph(name = 'flow_chart1') as s:
         s.attr(rank='same')
 
         while article_id_index < len(article_id_list):
-            cur_article_id_str = produce_graph_upper.list2str(article_id_list[article_id_index], 10, len(article_id_list))
+            cur_article_id_str = produce_graph_upper.list2str(article_id_list[article_id_index], 10,
+                                                              len(article_id_list))
             article_id_stack.append(IdNode(article_id_list[article_id_index], article_id_index + 1))
 
             # Create a node,
@@ -120,7 +121,8 @@ def and_or(article_id_list, operator_list):
                 article_id_stack.append(IdNode(cur_result, node_index))
 
                 # Create a node
-                dot.node(node_prefix + str(node_index), produce_graph_upper.list2str(sorted(cur_result), 10, len(article_id_list)))
+                dot.node(node_prefix + str(node_index),
+                         produce_graph_upper.list2str(sorted(cur_result), 10, len(article_id_list)))
 
                 # Connect node
                 dot.edge(node_prefix + str(cur_article_node1.index), node_prefix + str(node_index),
@@ -134,16 +136,17 @@ def and_or(article_id_list, operator_list):
                 operator_stack.append(operator_list[operator_index])
             operator_index += 1
             article_id_index += 1
-        
+
         while len(operator_stack) > 0:
             cur_article_node1 = article_id_stack.pop()
             cur_article_node2 = article_id_stack.pop()
             cur_operator = operator_stack.pop()
-            
+
             result = and_or_operation(cur_article_node1.id_list, cur_article_node2.id_list, cur_operator)
             article_id_stack.append(IdNode(result, node_index))
             # Create a node
-            dot.node(node_prefix + str(node_index), produce_graph_upper.list2str(sorted(result), 10, len(article_id_list)))
+            dot.node(node_prefix + str(node_index),
+                     produce_graph_upper.list2str(sorted(result), 10, len(article_id_list)))
             dot.edge(node_prefix + str(cur_article_node1.index), node_prefix + str(node_index),
                      color=OperationColor.OR.value if cur_operator == '|' else OperationColor.AND.value)
             dot.edge(node_prefix + str(cur_article_node2.index), node_prefix + str(node_index),
@@ -154,18 +157,27 @@ def and_or(article_id_list, operator_list):
     result_id = article_id_stack.pop()
 
     print_result(result_id.id_list)
-    # print(dot)
-    # dot.view()
 
+
+    with dot.subgraph(name='cluster_legend') as c:
+        c.graph_attr['label'] = "Legend"
+        c.node_attr['shape'] = "plaintext"
+        c.node("dummy_node_1","")
+        c.node("dummy_node_2", "")
+        c.edge('dummy_node_1', 'AND', color=OperationColor.AND.value)
+        c.edge('dummy_node_2', 'OR', color=OperationColor.OR.value)
+
+
+    # print(dot)
+    #dot.view()
 
     # dot: the graph
     # node_prefix + str(node_index + 1 if node_index != -1 else 1): the name of the last node
     # if we have only one node, the name of that node is node_prefix + str(1)
-
     return dot, node_prefix + str(node_index + 1 if node_index != -1 else 1), result_id.id_list
 
 
 if __name__ == '__main__':
-    index_list = [[1,2,3]]
-    operator_list = []
+    index_list = [[1, 2, 3],[4,5,6],[1,2,3,4,5,6,7,8],[2,4,6,8,10]]
+    operator_list = ['&','|','&']
     and_or(index_list, operator_list)
